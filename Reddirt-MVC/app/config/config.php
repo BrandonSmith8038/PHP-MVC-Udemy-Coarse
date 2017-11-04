d1a1f1c87db7b7d401ba3d5178eb12cb59bb084d
Brandon Smith <brandon@reddirtwebdesign.com>
2017-11-04 09:12:13 +0000

    Config File & Uploader

---
+++ Reddirt-MVC/app/config/config.php
@@ -0,0 +1,8 @@
+<?php
+
+    //APP Root
+    define('APPROOT', dirname(dirname(__FILE__)));
+    // URL Root
+    define('URLROOT', 'https://php-mvc-cowboy8038.c9users.io/Reddirt-MVC');
+    //Site Name
+    define('SITENAME', 'Reddirt-MVC');
