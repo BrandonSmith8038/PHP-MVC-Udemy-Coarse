48f7b687ccf0d9f87ccde73a67ebd13f225d57d4
Brandon Smith <brandon@reddirtwebdesign.com>
2017-11-04 11:55:16 +0000

    Database

--- Reddirt-MVC/app/config/config.php
+++ Reddirt-MVC/app/config/config.php
@@ -1,5 +1,14 @@
 <?php
 
+    
+    // DB Params
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', '');
+    define('DB_NAME', 'tmvc');
+    
+    
+    
     //APP Root
     define('APPROOT', dirname(dirname(__FILE__)));
     // URL Root
