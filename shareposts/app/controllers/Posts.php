ff957899858e080277e93fcdeff7701dcae7d6d0
Brandon Smith <brandon@reddirtwebdesign.com>
2017-11-10 10:06:36 +0000

    Deleting Posts

--- shareposts/app/controllers/Posts.php
+++ shareposts/app/controllers/Posts.php
@@ -141,6 +141,26 @@
             $this->view('posts/show', $data);
         }
         
+        public function delete($id){
+            if($_SERVER['REQUEST_METHOD'] == 'POST'){
+                // Get existing post from model
+                    $post = $this->postModel->getPostById($id);
+                    
+                    //Check for Owner
+                    if($post->user_id != $_SESSION['user_id']){
+                        redirect('posts');
+                    }
+                if($this->postModel->deletePost($id)){
+                    flash('post_message', 'Post Deleted');
+                    redirect('posts');
+                } else {
+                    die('Something Went Wrong');
+                }
+            } else {
+                redirect('posts');
+            }
+        }
+        
          
         
     }
